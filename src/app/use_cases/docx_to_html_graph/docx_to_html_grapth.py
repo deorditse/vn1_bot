de245a254992bbf341b98629ec6ffd3b8d883636
Dmitry <94318108+deorditse@users.noreply.github.com>
2026-01-09 18:02:58 +0300

    update test jupyter

--- src/app/use_cases/docx_to_html_graph/docx_to_html_grapth.py
+++ src/app/use_cases/docx_to_html_graph/docx_to_html_grapth.py
@@ -27,7 +27,6 @@ def generate_menu_html(state: GraphState) -> GraphState:
     print(f"generate_menu_html: {state}")
 
     return {
-        "mdFile": state["mdFile"],
         "html_menu": "html_menu",
     }
 
@@ -57,9 +56,6 @@ def summarize(state: GraphState) -> GraphState:
     # =======================================================
 
 
-llm = LLMService().openai()
-
-
 class ValidationRouter:
     def __init__(self, max_attempts: int = 3):
         self.max_attempts = max_attempts
@@ -76,12 +72,11 @@ class ValidationRouter:
         return "generate_content_html"
 
 
-async def build_docx_to_html_graph(file: bytes) -> Dict:
+llm = LLMService().openai()
+
+
+def build_graph() -> StateGraph:
     # convert file to MD
-    md = '''create todo: MD convert'''
-    initState = {
-        "mdFile": md,
-    }
     graph = StateGraph(GraphState)
 
     graph.add_node("generate_menu_html", generate_menu_html)
@@ -106,8 +101,51 @@ async def build_docx_to_html_graph(file: bytes) -> Dict:
 
     graph.add_edge("summarize", END)
 
-    graph.set_entry_point("generate_menu_html")
-    graph.set_finish_point("summarize")
-    app = graph.compile()
-    result_generated = await app.ainvoke(initState)
-    return result_generated
+    # graph.set_entry_point("generate_menu_html")
+    # graph.set_finish_point("summarize")
+
+    return graph
+
+
+md_to_html_graph = build_graph()
+
+compile_md_to_html_graph = md_to_html_graph.compile()
+
+#
+#
+# async def build_docx_to_html_graph(file: bytes) -> Dict:
+#     # convert file to MD
+#     md = '''create todo: MD convert'''
+#     initState = {
+#         "mdFile": md,
+#     }
+#     graph = StateGraph(GraphState)
+#
+#     graph.add_node("generate_menu_html", generate_menu_html)
+#     graph.add_node("generate_content_html", generate_content_html)
+#     graph.add_node("validate_content_html", validate_content_html)
+#     graph.add_node("summarize", summarize)
+#
+#     graph.add_edge(START, "generate_menu_html")
+#     graph.add_edge('generate_menu_html', "summarize")
+#
+#     graph.add_edge(START, "generate_content_html")
+#     graph.add_edge("generate_content_html", "validate_content_html")
+#
+#     graph.add_conditional_edges(
+#         "validate_content_html",
+#         ValidationRouter(),
+#         {
+#             "generate_content_html": "generate_content_html",
+#             "summarize": "summarize",
+#         },
+#     )
+#
+#     graph.add_edge("summarize", END)
+#
+#     graph.set_entry_point("generate_menu_html")
+#     graph.set_finish_point("summarize")
+#     app = graph.compile()
+#
+#     result_generated = await app.ainvoke(initState)
+#     return result_generated
