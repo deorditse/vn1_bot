2bd62d4ec929715c5f1af46344ea8a66ef2f9cbd
Dmitry <94318108+deorditse@users.noreply.github.com>
2026-01-21 03:58:56 +0300

    vertsion

--- src/app/api/routers/converter.py
+++ src/app/api/routers/converter.py
@@ -2,9 +2,9 @@ import traceback
 from fastapi import APIRouter, Body, File, UploadFile
 from fastapi.responses import JSONResponse
 from starlette import status
-
 from app.use_cases.docx_to_html_graph.docx_to_html import ToHtmlConverterUseCase
 from infrastructure.converters.docx_to_md_converter import DocxToMdConverter
+from app import __version__
 
 router = APIRouter()
 
@@ -12,7 +12,7 @@ router = APIRouter()
 @router.get("/")
 async def check():
     return JSONResponse(
-        {"ok": True, "message": "Converter service is running."},
+        {"ok": True, "message": f"Converter service is running. version: ${__version__}"},
         status_code=status.HTTP_200_OK,
     )
 
